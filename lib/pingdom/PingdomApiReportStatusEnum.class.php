f5df4040c552bef421d60ec871f1d7efb2b3ad87
tuebernickel <tuebernickel@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-07-31 13:15:22 -0500

    added report status and report via enum classes
    git-svn-id: http://svn.symfony-project.com/plugins/wspPingdomPlugin/trunk@20648 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/pingdom/PingdomApiReportStatusEnum.class.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author        Toni Uebernickel <toni@uebernickel.info>
+ * @link          http://toni.uebernickel.info/
+ *
+ * @package       wspPingdomPlugin
+ * @subpackage    pingdom.lib
+ * @version       $Id$
+ * @link          $HeadURL$
+ */
+
+/**
+ * Enum class for defining status of report functions.
+ *
+ * @see http://www.pingdom.com/services/api-documentation/class_Report_statusEnum
+ */
+class PingdomApiReportStatusEnum
+{
+  const SENT = 'SENT';
+
+  const DELIVERED = 'DELIVERED';
+
+  const NOT_DELIVERED = 'NOT_DELIVERED';
+
+  const NO_CREDITS = 'NO_CREDITS';
+
+  const ERROR = 'ERROR';
+
+  /**
+   * Get the list of all values of this Enum.
+   *
+   * @return array
+   */
+  public static final function getEnum() {
+    return array(
+      self::SENT,
+      self::DELIVERED,
+      self::NOT_DELIVERED,
+      self::NO_CREDITS,
+      self::ERROR,
+    );
+  }
+}
---
+++ lib/pingdom/PingdomApiReportViaEnum.class.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author        Toni Uebernickel <toni@uebernickel.info>
+ * @link          http://toni.uebernickel.info/
+ *
+ * @package       wspPingdomPlugin
+ * @subpackage    pingdom.lib
+ * @version       $Id$
+ * @link          $HeadURL$
+ */
+
+/**
+ * Enum class for defining status of report functions.
+ *
+ * @see http://www.pingdom.com/services/api-documentation/class_Report_viaEnum
+ */
+class PingdomApiReportViaEnum
+{
+  const EMAIL = 'EMAIL';
+
+  const SMS = 'SMS';
+
+  /**
+   * Get the list of all values of this Enum.
+   *
+   * @return array
+   */
+  public static final function getEnum() {
+    return array(
+      self::EMAIL,
+      self::SMS,
+    );
+  }
+}
