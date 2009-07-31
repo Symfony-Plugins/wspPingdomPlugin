4f33e179cde2e705f87d2489206fdc62b163d6dd
tuebernickel <tuebernickel@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-07-31 17:02:49 -0500

    added missing class: PingdomApiReportCheckStateEnum (fixes #6942)
    git-svn-id: http://svn.symfony-project.com/plugins/wspPingdomPlugin/trunk@20661 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/pingdom/PingdomApiReportCheckStateEnum.class.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author        Toni Uebernickel <toni@uebernickel.info>
+ * @link          http://toni.uebernickel.info/
+ *
+ * @package       wspPingdomPlugin
+ * @subpackage    pingdom.lib
+ * @version       $Id$
+ * @link          $HeadURL$
+ */
+
+/**
+ * Enum class for defining check states of report responses.
+ *
+ * @see http://www.pingdom.com/services/api-documentation/class_CheckState
+ */
+class PingdomApiReportCheckStateEnum
+{
+  const CHECK_UP = 'CHECK_UP';
+
+  const CHECK_DOWN = 'CHECK_DOWN';
+
+  const CHECK_UNKNOWN = 'CHECK_UNKNOWN';
+
+  /**
+   * Get the list of all values of this Enum.
+   *
+   * @return array
+   */
+  public static final function getEnum() {
+    return array(
+      self::CHECK_UP,
+      self::CHECK_DOWN,
+      self::CHECK_UNKNOWN,
+    );
+  }
+}
